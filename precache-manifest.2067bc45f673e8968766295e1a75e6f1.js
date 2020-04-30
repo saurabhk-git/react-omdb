76ce67c2f59fbf370bb1c438bd2cdb3dbee38181
Saurabh Kumar <saurabhk.git@gmail.com>
2020-04-30 13:29:18 +0530

    Updates

---
+++ precache-manifest.2067bc45f673e8968766295e1a75e6f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0413838e282ba4e52040ca897c8969",
+    "url": "/react-omdb/index.html"
+  },
+  {
+    "revision": "353a03b7891dbb7858d1",
+    "url": "/react-omdb/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "73453b6ae165b79eafc8",
+    "url": "/react-omdb/static/css/main.f4c5ba14.chunk.css"
+  },
+  {
+    "revision": "353a03b7891dbb7858d1",
+    "url": "/react-omdb/static/js/2.39693fc4.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/react-omdb/static/js/2.39693fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73453b6ae165b79eafc8",
+    "url": "/react-omdb/static/js/main.dec02f74.chunk.js"
+  },
+  {
+    "revision": "a1c5362cc1dc45160b0c",
+    "url": "/react-omdb/static/js/runtime-main.8237376b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-omdb/static/media/logo.5d5d9eef.svg"
+  }
+]);
