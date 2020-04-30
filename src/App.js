d1569759c57de7569fe4239b591d469ba9074c8b
Saurabh Kumar <saurabhk.git@gmail.com>
2020-04-30 13:10:48 +0530

    new local repository

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import $ from 'jquery';
+import axios from 'axios';
+
+
+function App() {
+  return (
+    <div className="App">
+       <div class="container">
+      <header className="App-header">
+        <h2>Movie List App</h2>
+        <h5>React JS Application using OMDB API </h5> 
+        <img src={logo} className="App-logo" alt="logo" />
+      </header> 
+     
+        <div class="jumbotron">  
+          <h3>Search for any Movie</h3>          
+          <form id="searchForm">
+          <input className="form-control glyphicon glyphicon-search" type="text"  id="searchText" placeholder="Search Movies..."/> 
+          </form>   
+        </div>
+      </div>
+        <br/>
+        <div className="container">
+        <div id="movies" className="row"></div>
+        </div> 
+      
+    </div>
+    
+  );
+}
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
+
+function getMovies(searchText){
+  axios.get('http://www.omdbapi.com?s='+searchText+'&apikey=31ac049d')
+    .then((response) => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col-md-3">
+            <div class="well text-center">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+          </div>
+        `;
+      });
+
+      $('#movies').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+/// Selected Movie  ///
+function movieSelected(id){
+  sessionStorage.setItem('movieId', id);
+  window.location = 'movie.html';
+  return false;
+}
+
+function getMovie(){
+  let movieId = sessionStorage.getItem('movieId');
+
+  axios.get('http://www.omdbapi.com?i='+movieId+'&apikey=31ac049d')
+    .then((response) => {
+      console.log(response);
+      let movie = response.data;
+
+      let output =`
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <div class="well">
+            <h3>Plot</h3>
+            ${movie.Plot}
+            <hr>
+            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+            <a href="index.html" class="btn btn-default">Go Back To Search</a>
+          </div>
+        </div>
+      `;
+
+      $('#movie').html(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+
+
+export default App;
